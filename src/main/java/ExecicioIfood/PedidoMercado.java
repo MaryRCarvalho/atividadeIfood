5d3b6162c0f19b8925ac85caf25bfc1e3fe46f03
Marilia Carvalho <marilia.carvalho@delage.com.br>
2025-04-29 14:51:02 -0300

    atividade ifood 5

--- src/main/java/ExecicioIfood/PedidoMercado.java
+++ src/main/java/ExecicioIfood/PedidoMercado.java
@@ -1,4 +1,74 @@
 package ExecicioIfood;
 
-public class PedidoMercado {
+import java.util.Observable;
+
+public class PedidoMercado extends Observable implements IPedido{
+
+
+    private String itemPedido;
+    private PedidoEstado estado;
+
+    public void setEstado(PedidoEstado estado) {
+        this.estado = estado;
+    }
+
+    public PedidoEstado getEstado() {
+        return estado;
+    }
+
+    public void setItemPedido(String itemPedido){
+        this.itemPedido = itemPedido;
+    }
+
+    public String getItemPedido() {
+        return itemPedido;
+    }
+
+    private void notificarCliente() {
+        setChanged();
+        notifyObservers();
+    }
+
+    @Override
+    public void pagamentoValidado() {
+        boolean resultado = estado.validarPagamento(this);
+        if (resultado == true) {
+            notificarCliente();
+        }
+    }
+
+    @Override
+    public void preparando() {
+
+        boolean resultado = estado.prepararPedido(this);
+        if (resultado == true) {
+            notificarCliente();
+        }
+    }
+
+    @Override
+    public void entregando() {
+
+        boolean resultado = estado.rotaDeEntrega(this);
+        if (resultado == true) {
+            notificarCliente();
+        }
+    }
+
+    @Override
+    public void entregue() {
+        boolean resultado = estado.finalizar(this);
+        if (resultado == true) {
+            notificarCliente();
+        }
+    }
+
+    @Override
+    public void cancelado() {
+
+        boolean resultado = estado.cancelar(this);
+        if (resultado == true) {
+            notificarCliente();
+        }
+    }
 }
