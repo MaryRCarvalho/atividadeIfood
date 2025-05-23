6a671a8875c98e6fb20a680670e1433c0cb9cd73
Marilia Carvalho <marilia.carvalho@delage.com.br>
2025-05-23 09:38:00 -0300

    teste finalizados

--- src/test/java/ExecicioIfood/PedidoTest.java
+++ src/test/java/ExecicioIfood/PedidoTest.java
@@ -154,5 +154,97 @@ class PedidoTest {
         assertEquals ("Pedido cancelado", pedido.getEstado().getEstado());
         assertEquals ("Ana, seu pedido está: Pedido cancelado", cliente.getUltimaNotificacao());
     }
+
+
+    @Test
+    void ValidarPagamentoValidoQuandoFinalizado(){
+        pedido.setEstado(PedidoEstadoEntregue.getInstance());
+        cliente.acompanhar(pedido);
+        pedido.pagamentoValidado();
+        assertEquals ("Pedido entregue", pedido.getEstado().getEstado());
+        assertNull(cliente.getUltimaNotificacao());
+    }
+
+    @Test
+    void ValidarEmPreparoQuandoFinalizado(){
+        pedido.setEstado(PedidoEstadoEntregue.getInstance());
+        cliente.acompanhar(pedido);
+        pedido.preparando();
+        assertEquals ("Pedido entregue", pedido.getEstado().getEstado());
+        assertNull(cliente.getUltimaNotificacao());
+    }
+
+    @Test
+    void ValidarEmEntregaQuandoFinalizado(){
+        pedido.setEstado(PedidoEstadoEntregue.getInstance());
+        cliente.acompanhar(pedido);
+        pedido.entregando();
+        assertEquals ("Pedido entregue", pedido.getEstado().getEstado());
+        assertNull(cliente.getUltimaNotificacao());
+    }
+
+    @Test
+    void ValidarFinalizadoQuandoFinalizado(){
+        pedido.setEstado(PedidoEstadoEntregue.getInstance());
+        cliente.acompanhar(pedido);
+        pedido.entregue();
+        assertEquals ("Pedido entregue", pedido.getEstado().getEstado());
+        assertNull(cliente.getUltimaNotificacao());
+    }
+
+    @Test
+    void ValidarCanceladoQuandoFinalizado(){
+        pedido.setEstado(PedidoEstadoEntregue.getInstance());
+        cliente.acompanhar(pedido);
+        pedido.cancelado();
+        assertEquals ("Pedido entregue", pedido.getEstado().getEstado());
+        assertNull(cliente.getUltimaNotificacao());
+    }
+
+
+    @Test
+    void ValidarPagamentoValidoQuandoCancelado(){
+        pedido.setEstado(PedidoEstadoCancelado.getInstance());
+        cliente.acompanhar(pedido);
+        pedido.pagamentoValidado();
+        assertEquals ("Pedido cancelado", pedido.getEstado().getEstado());
+        assertNull(cliente.getUltimaNotificacao());
+    }
+
+    @Test
+    void ValidarEmPreparoQuandoCancelado(){
+        pedido.setEstado(PedidoEstadoCancelado.getInstance());
+        cliente.acompanhar(pedido);
+        pedido.preparando();
+        assertEquals ("Pedido cancelado", pedido.getEstado().getEstado());
+        assertNull(cliente.getUltimaNotificacao());
+    }
+
+    @Test
+    void ValidarEmEntregaQuandoCancelado(){
+        pedido.setEstado(PedidoEstadoCancelado.getInstance());
+        cliente.acompanhar(pedido);
+        pedido.entregando();
+        assertEquals ("Pedido cancelado", pedido.getEstado().getEstado());
+        assertNull(cliente.getUltimaNotificacao());
+    }
+
+    @Test
+    void ValidarFinalizadoQuandoCancelado(){
+        pedido.setEstado(PedidoEstadoCancelado.getInstance());
+        cliente.acompanhar(pedido);
+        pedido.entregue();
+        assertEquals ("Pedido cancelado", pedido.getEstado().getEstado());
+        assertNull(cliente.getUltimaNotificacao());
+    }
+
+    @Test
+    void ValidarCanceladoQuandoCancelado(){
+        pedido.setEstado(PedidoEstadoCancelado.getInstance());
+        cliente.acompanhar(pedido);
+        pedido.cancelado();
+        assertEquals ("Pedido cancelado", pedido.getEstado().getEstado());
+        assertNull(cliente.getUltimaNotificacao());
+    }
 //fazer para todos os estados possíveis
 }
